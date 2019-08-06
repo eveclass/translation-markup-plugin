0fd7216e3ffbc0b4a8c1e40fab857ea91a607af8
Mario <mario@macbook-mario.local>
2019-08-06 10:26:17 -0300

    removing changed files check

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 Webpack plugin that uses [Translation Markup](https://translationmarkup.com/) to auto generate you translations files.
 
+## Getting Started
+
+Translation Markup Plugin auto generates your translations files, write your transaltions only once, in YAML, and refernce the auto generated JS files to internationalize your application with any i18n library you desire.
+
+## Install
+
+### Install
+
+**NPM:**
+
+```sh
+npm install @shiftcode/translation-markup-plugin
+```
+
+**Yarn:**
+
+```sh
+yarn add @shiftcode/translation-markup-plugin
+```
+
+## Config
+
 `webpack.config.js`
 
 ```
--- src/plugin.ts
+++ src/plugin.ts
@@ -10,69 +10,44 @@ class TranslationMarkupPlugin {
   private readonly globPath: string;
   private readonly outputDirectory: string;
   private readonly options: ICompileOptions;
-  private readonly test: string;
 
   constructor(params?: IPluginConstructorParams) {
     const {
       globPath,
       outputDirectory,
-      options = { format: FormatOptions.JS },
-      test = '.lang.yaml'
+      options = { format: FormatOptions.JS }
     } = { ...params };
 
     this.globPath = globPath;
     this.outputDirectory = outputDirectory;
     this.options = options;
-    this.test = test;
   }
 
   public apply(compiler: any) {
     /**
-     * Used in the build to production
+     * Used in the build stage
      */
     compiler.hooks.afterPlugins.tap('TranslationMarkupPlugin', () => {
-      compile({
-        globPath: this.globPath,
-        outputDirectory: this.outputDirectory,
-        options: this.options
-      }).catch((error: any) => {
-        console.warn('Translation Compile error');
-        console.log(error);
-      });
+      this.compileTranslationFiles();
     });
 
     /**
      * Used in development watch mode
      */
-    compiler.hooks.watchRun.tapPromise(
-      'TranslationMarkupPlugin',
-      async (runCompiler: any) => {
-        const changedFilesPaths = Object.keys(
-          runCompiler.watchFileSystem.watcher.mtimes
-        );
+    compiler.hooks.watchRun.tapPromise('TranslationMarkupPlugin', async () => {
+      this.compileTranslationFiles();
+    });
+  }
 
-        const translationsToCompile: Promise<void>[] = [];
-
-        for (const filePath of changedFilesPaths) {
-          if (filePath.includes(this.test)) {
-            translationsToCompile.push(
-              compile({
-                globPath: filePath,
-                outputDirectory: this.outputDirectory,
-                options: this.options
-              })
-            );
-          }
-        }
-
-        try {
-          await Promise.all(translationsToCompile);
-        } catch (error) {
-          console.warn('Translation Compile error');
-          console.log(error);
-        }
-      }
-    );
+  private compileTranslationFiles() {
+    compile({
+      globPath: this.globPath,
+      outputDirectory: this.outputDirectory,
+      options: this.options
+    }).catch((error: any) => {
+      console.warn('Translation Compile error');
+      console.log(error);
+    });
   }
 }
 
